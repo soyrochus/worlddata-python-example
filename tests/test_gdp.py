8f7836d38812689b8107d90f7ac317a3cd076666
Iwan van der Kleijn <ivanderk@capgemini.com>
2019-07-15 22:21:20 +0200

    TempSave

--- tests/test_gdp.py
+++ tests/test_gdp.py
@@ -16,8 +16,14 @@ def test_gdp_get_countries():
     assert '533' == countries[0].UNCode
     assert 'ZWE' == countries[-1].CountryCode # Last item in list: Zimbabwe
 
-def test_gdp_get_gdp():
+def test_gdp_get_all():
     gdp = GdpData(DB_PATH)
-    gdp = gdp.get_gdp()
-    assert 15576 == len(gdp) # total number of countries
+    data = gdp.get_all()
+
+    assert 264 == len(data.keys()) # total number of countries
+    
+    total = 0
+    for e in data.values():
+        total += len(e.values()) 
+    assert 15576 == total
     
